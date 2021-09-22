50c16ad5ee457bd0f8e69c4df627b723cfbac15b
Gregory Lira <gregory.lira007@gmail.com>
2021-09-22 14:02:06 -0300

    Adicionando listagem de todos os elogios mandados e refaturação do compliments

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserReceiveComplimentController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListUserSendComplimentController } from "./controllers/ListUserSendComplimentsController";
 
 const router = Router();
 
@@ -12,10 +14,15 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentController();
+const listUserSendComplimentController = new ListUserSendComplimentController();
 
 router.post("/tags",ensureAuthenticated,ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments",ensureAuthenticated, createComplimentController.handle);
+router.get("/users/compliments/send",ensureAuthenticated, listUserSendComplimentController.handle )
+router.get("/users/compliments/receive",ensureAuthenticated, listUserReceiveComplimentsController.handle )
+
 
 export { router }
